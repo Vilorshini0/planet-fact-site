9b555f1d0e48ebc133bc07e9e975cad6e2ca0430
Vilorshini <ramsamyvilorshini@gmail.com>
2024-05-11 23:22:29 +0400

    [INTE] Planet pages & Component planetDescription
    Mercury and Venus

---
+++ src/component/PlanetDescription.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import wikiIcon from '../assets/icon-source.svg'
+
+export default function PlanetDescription(props) {
+    return (
+        <section>
+            <h1>{props.planetName}</h1>
+            <p>{props.description}</p>
+            <p className="link">
+                Source: <a href={props.source}>Wikipedia </a>
+                <img src={wikiIcon} alt="" />
+            </p>
+        </section>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './scss/app.css'
+import React from "react"
+import { createRoot } from 'react-dom/client'
+import App from "./App"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Mercury from "./pages/Mercury"
+import Venus from "./pages/Venus"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Mercury />,
+      },
+      {
+        path: "/venus",
+        element: <Venus />,
+      }
+    ],
+  }
+])
+
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
