b8f802bd90f592fbf701cdc0a1f5a518085ba3eb
Vilorshini <ramsamyvilorshini@gmail.com>
2024-05-12 23:01:23 +0400

    [INTE] Dynamic description and CTA condition

--- src/pages/Venus.jsx
+++ src/pages/Venus.jsx
@@ -2,30 +2,69 @@ import React from "react";
 import { useSelector } from "react-redux"
 import imgPlanet from '../assets/planet-venus.svg'
 import imgPlanetInternal from '../assets/planet-venus-internal.svg'
+import imgPlanetSurface from '../assets/geology-venus.png'
 import PlanetDescription from "../component/PlanetDescription";
 import PlanetCharacteristics from "../component/PlanetCharacteristics";
 import data from "../data.json"
-  
-  export default function Venus() {
-    const screenWidth = useSelector(state => state.appState.screenWidth)
-    const [myPlanetImage, setMyPlanetImage] = React.useState(imgPlanet)
 
-    function changeToOverview() {
-      setMyPlanetImage(imgPlanet)
+export default function Venus() {
+  const screenWidth = useSelector(state => state.appState.screenWidth)
+  const [pageStatus, setPageStatus] = React.useState('overview')
+  const venus = data[1]
+
+  const pageStatusMapping = {
+    overview: {
+      content: venus.overview.content,
+      source: venus.overview.source,
+      img: imgPlanet
+    },
+    structure: {
+      content: venus.structure.content,
+      source: venus.structure.source,
+      img: imgPlanetInternal
+    },
+    geology: {
+      content: venus.geology.content,
+      source: venus.geology.source,
+      img: imgPlanet
     }
+  }
 
-    function changeToInternalStructure() {
-      setMyPlanetImage(imgPlanetInternal)
-    }
+  return (
+    <main className="planet-venus">
+      {/* Mobile */}
+      {screenWidth <= 768 &&
+        <section>
+          <button onClick={() => setPageStatus('overview')} className={pageStatus === 'overview' ? 'active' : undefined }>Overview</button>
+          <button onClick={() => setPageStatus('structure')} className={pageStatus === 'structure' ? 'active' : undefined }>Structure</button>
+          <button onClick={() => setPageStatus('geology')} className={pageStatus === 'geology' ? 'active' : undefined }>Surface</button>
+        </section>
+      }
 
-    return (
-        <main className="planet-venus">
-            {screenWidth}
-            <img src={myPlanetImage} alt="" />
-            <PlanetDescription planetName={data[1].name} description={data[1].overview.content} source={data[1].overview.source}/>
-            <button onClick={changeToOverview}>Overview</button>
-            <button onClick={changeToInternalStructure}>Internal Structure</button>
-            <PlanetCharacteristics rotationTime={data[1].rotation} revolutionTime={data[1].revolution} radius={data[1].radius} temperature={data[1].temperature}/>
-        </main>
-    )
+      <img src={pageStatusMapping[pageStatus]?.img} />
+      {pageStatus === 'geology' && <img src={imgPlanetSurface} />}
+
+      <PlanetDescription
+        planetName={venus.name}
+        description={pageStatusMapping[pageStatus]?.content || ''}
+        source={pageStatusMapping[pageStatus]?.source || ''}
+      />
+
+      {/* Tablet/desktop */}
+      {screenWidth > 768 &&
+        <section>
+          <button onClick={() => setPageStatus('overview')} className={pageStatus === 'overview' ? 'active' : undefined }>Overview</button>
+          <button onClick={() => setPageStatus('structure')} className={pageStatus === 'structure' ? 'active' : undefined }>Internal Structure</button>
+          <button onClick={() => setPageStatus('geology')} className={pageStatus === 'geology' ? 'active' : undefined }>Surface</button>
+        </section>
+      }
+
+      <PlanetCharacteristics
+        rotationTime={venus.rotation}
+        revolutionTime={venus.revolution}
+        radius={venus.radius}
+        temperature={venus.temperature}
+      />
+    </main>
+  )
 }
